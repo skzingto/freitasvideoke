1b42ee760f8d4d3bf5137efc550dc8e64332df91
skzingto <47371164+skzingto@users.noreply.github.com>
2025-07-31 13:06:05 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-alert('FAQ carregando!');
-
 document.querySelectorAll('.faq-question').forEach(button => {
     button.addEventListener('click', () => {
         const answer = button.nextElementSibling;
